27fad710c0aaafa368b312bd1f5a214a77d38e1f
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-02-28 15:27:03 +0100

    grid completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,25 +17,20 @@ container.append(row);
 container.classList.add('container');
 row.classList.add('row');
 
+
 //Creo un ciclo che mi permette di creare 100 elementi
 for (let i = 0; i < 100; i++) {
     // Creo una variabile che si incrementa assieme all'indice
     //(Parte da 1 anziché da zero perché dovrò visualizzarla nella card)
     const my_counter= i+1;
     //Creo un elemento div
-    const card = document.createElement('div')
-    //Creo un elemento div
     const col = document.createElement('div');
     // Appendo il primo a row
     row.append(col);
     // Assegno la classe col all'elemento del DOM
     col.classList.add('col')
-    // Appendo il secondo a col
-    col.append(card);
-    // Assegno la classe col all'elemento del DOM
-    card.classList.add('card')
 
-    card.innerHTML = `${my_counter}`
+    col.innerHTML = `${my_counter}`
 // Verifico se l'incremento è corretto
 //onsole.log(my_counter); //OK
 }
